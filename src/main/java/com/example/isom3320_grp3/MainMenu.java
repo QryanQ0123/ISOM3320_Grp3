197a39a58c2c2fae7cce21cf151171bc9ca3af2c
QryanQ <hiulonglee@gmail.com>
2025-05-25 17:07:58 +0800

    Add Account Scene

--- src/main/java/com/example/isom3320_grp3/MainMenu.java
+++ src/main/java/com/example/isom3320_grp3/MainMenu.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -15,6 +16,10 @@ public class MainMenu extends Application {
         VBox vbox = new VBox(10);
         vbox.setAlignment(Pos.CENTER);
 
+        //Primary scene (Main Menu)
+        Scene primaryScene = new Scene(vbox);
+        primaryStage.setScene(primaryScene);
+
         //buttons
         Button btCreateAccount =  new Button("Create Account");
         Button btCreateType = new Button("Create Type");
@@ -22,7 +27,11 @@ public class MainMenu extends Application {
         Button btDisplayTransaction = new Button("Display Transaction");
 
         //ActionEvent Test
-        btCreateAccount.setOnAction(e -> {System.out.println("Create Account");});
+        btCreateAccount.setOnAction(e -> {
+            //new scene for create account
+            Scene accountScene = createAccountScene(primaryStage, primaryScene);
+            primaryStage.setScene(accountScene);
+        });
         btCreateType.setOnAction(e -> {System.out.println("Create Type");});
         btCreateTransaction.setOnAction(e -> {System.out.println("Create Transaction");});
         btDisplayTransaction.setOnAction(e -> {System.out.println("Display Transaction");});
@@ -30,13 +39,38 @@ public class MainMenu extends Application {
         //Add to VBox
         vbox.getChildren().addAll(btCreateAccount, btCreateType, btCreateTransaction, btDisplayTransaction);
 
-        //Scene
-        Scene scene = new Scene(vbox);
-        primaryStage.setScene(scene);
+        //show
         primaryStage.show();
     }
-
     public static void main(String[] args) {
         launch();
     }
+
+    //method for createAccount
+    private Scene createAccountScene(Stage primaryStage , Scene menuScene) {
+        BorderPane accountMenu = new BorderPane();
+        VBox buttonBox = new VBox(10);
+        buttonBox.setAlignment(Pos.CENTER);
+
+        Button btAccountName = new Button("Account Name");
+        Button btCurrencyType = new Button("Currency Type");
+        Button btInitialBalance = new Button("Initial Balance");
+        Button btBackToMenu = new Button("Back");
+        // Add actionevents
+        btAccountName.setOnAction(e -> System.out.println("Account Name"));
+        btCurrencyType.setOnAction(e -> System.out.println("Currency Type"));
+        btInitialBalance.setOnAction(e -> System.out.println("Initial Balance"));
+        btBackToMenu.setOnAction(e ->
+                primaryStage.setScene(menuScene)
+                );
+        // Add buttons t0 account menu
+        buttonBox.getChildren().addAll(btAccountName, btCurrencyType, btInitialBalance);
+        accountMenu.setCenter(buttonBox);
+        accountMenu.setBottom(btBackToMenu);
+        BorderPane.setAlignment(btBackToMenu, Pos.BOTTOM_LEFT);
+        // Create and return new scene
+        return new Scene(accountMenu);
+    }
+
+
 }
